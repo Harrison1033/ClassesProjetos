a8d475d21286a72f3e8756b0b798080baed3b944
Harrison de Oliveira Rodrigues <95246993+Harrison1033@users.noreply.github.com>
2023-01-10 09:18:01 -0300

    Modificação da classe3 List1

--- codigoJava/Coleções/List1.java
+++ codigoJava/Coleções/List1.java
@@ -6,10 +6,17 @@ import java.util.List;
 public class Main{
     public static void main(String[] args) {
         List<String> nomes = new ArrayList<>(16);
+        List<String> nomes1 = new ArrayList<>(16);
         nomes.add("Keliane");
         nomes.add("Harrison");
         nomes.add("Goldofredo");
         nomes.add("Astoufo");
+        nomes1.add("Lima");
+        nomes1.add("Oliveira");
+        nomes.remove("Astoufo");//Remove pelo equals
+        nomes.remove(2);//Remove pelo índice
+        nomes.addAll(nomes1);//Adiciona todos os elementos de nomes1 para nomes
+
 
         for (String nome: nomes){
             System.out.println(nome);
