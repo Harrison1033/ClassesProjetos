4e01d9b56ce003cbac42d2ef541021bd6127868f
Harrison de Oliveira Rodrigues <95246993+Harrison1033@users.noreply.github.com>
2023-02-04 09:40:15 -0300

    Criação da classDoisBarco

---
+++ codigoJava/Generics/Classe Genérica/ClasseDoisBarco.java
@@ -0,0 +1,14 @@
+package teste;
+
+public class ClasseDois {
+    private String nome;
+
+    public ClasseDois(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Barco{" + "nome='" + nome + '\'' + '}';
+    }
+}
