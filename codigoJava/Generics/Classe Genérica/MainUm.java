0575127cd5926000c4d98a998ca243cddf3548fe
Harrison de Oliveira Rodrigues <95246993+Harrison1033@users.noreply.github.com>
2023-02-07 14:41:14 -0300

    Criaçõa da classe MainUm

---
+++ codigoJava/Generics/Classe Genérica/MainUm.java
@@ -0,0 +1,23 @@
+package teste;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainUm {
+    public static void main(String[] args) {
+        List<ClasseUm>carrosDisponiveis = new ArrayList<>(List.of(new ClasseUm("BMW"),new ClasseUm("Fusca")));
+        List<ClasseDois>barcosDisponiveis = new ArrayList<>(List.of(new ClasseDois("Lancha"),new ClasseDois("Canoa")));
+
+        ClasseTres<ClasseUm>umClasseTres = new ClasseTres<>(carrosDisponiveis);
+        ClasseUm carro = umClasseTres.buscandoObjetoDisponivel();
+        System.out.println("Usando o carro por um mês...");
+        umClasseTres.retornaObjetoAlugado(carro);
+
+        System.out.println("____________________________________");
+
+        ClasseTres<ClasseDois>doisClasseTres = new ClasseTres<>(barcosDisponiveis);
+        ClasseDois barco = doisClasseTres.buscandoObjetoDisponivel();
+        System.out.println("Usando o barco por um mês...");
+        doisClasseTres.retornaObjetoAlugado(barco);
+    }
+}
