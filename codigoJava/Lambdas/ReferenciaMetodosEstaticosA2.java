6e19e363770bd6ca71838b5d393d6012c25d683a
Harrison de Oliveira Rodrigues <95246993+Harrison1033@users.noreply.github.com>
2023-02-21 18:43:41 -0300

    Criação da classe ReferenciaMetodosEstaticosA2

---
+++ codigoJava/Lambdas/ReferenciaMetodosEstaticosA2.java
@@ -0,0 +1,13 @@
+package teste;
+//ReferenciaMetodosEstaticosA2
+//AnimeComparators
+public class ClasseDois {
+    public static int compareByTitle(ClasseUm a1, ClasseUm a2){
+        return a1.getTitle().compareTo(a2.getTitle());
+    }
+    public static int compareByEpisodes(ClasseUm a1, ClasseUm a2){
+        return Integer.compare(a1.getEpisodes(),a2.getEpisodes());
+    }
+}
+
+
