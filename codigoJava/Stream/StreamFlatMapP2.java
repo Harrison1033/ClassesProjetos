382225ee0c2dd8d3691df0e23a25d9057b6aa905
Harrison de Oliveira Rodrigues <95246993+Harrison1033@users.noreply.github.com>
2023-03-09 11:48:59 -0300

    Criação da classe StreamFlatMapP2

---
+++ codigoJava/Stream/StreamFlatMapP2.java
@@ -0,0 +1,24 @@
+package teste;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+//import java.util.stream.Stream;
+
+//StreamFlatMapP2
+public class MainUm {//Mostra as letras de todas as palavras
+
+    public static void main(String[] args) {
+        List<String> words = List.of("Gomu", "Gomu", "No", "Mi");
+        String[] letters = words.get(0).split("");
+        System.out.println(Arrays.toString(letters));//Mostra as letras de uma só palavra
+
+//        List<String[]> collect = words.stream().map(w -> w.split("")).collect(Collectors.toList());
+//        Stream<String> stream = Arrays.stream(letters);
+        List<String> letters1 = words.stream()
+                .map(w -> w.split(""))
+                .flatMap(Arrays::stream)
+                .collect(Collectors.toList());
+        System.out.println(letters1);
+    }
+}
