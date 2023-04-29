f8641b39c865a805bb6af17fafd145fa6427ab65
Harrison de Oliveira Rodrigues <95246993+Harrison1033@users.noreply.github.com>
2023-04-29 18:57:55 -0300

    Modificação da classe Antecessor

--- codigoJava/antecessor.java
+++ codigoJava/antecessor.java
@@ -5,16 +5,16 @@ import java.util.Scanner;
 
 public class TesteDois {
     Scanner scan = new Scanner(System.in);
-    int numero;
-    int antecessor;
-    int sucessor;
+    int number;
+    int predecessor;
+    int successor;
 
     public void imprimir(){
-        System.out.println("Digite um número: ");
-        numero = scan.nextInt();
-        antecessor = numero - 1;
-        sucessor = numero + 1;
-        System.out.println("O número é: " + numero + "\nO antecessor é: " + antecessor + "\nO sucessor é: " + sucessor);
+        System.out.println("Enter a number: ");
+        number = scan.nextInt();
+        predecessor = number - 1;
+        successor = number + 1;
+        System.out.println("The number is: " + number + "\nThe predecessor é: " + predecessor + "\nThe successor é: " + successor);
 
     }
 
