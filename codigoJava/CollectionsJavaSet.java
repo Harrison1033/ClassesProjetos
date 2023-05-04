472dedb0a5925c0651afc1bc649bf939b6b304f6
Harrison de Oliveira Rodrigues <95246993+Harrison1033@users.noreply.github.com>
2023-05-04 12:23:58 -0300

    Refatoração da classe

--- codigoJava/CollectionsJavaSet.java
+++ codigoJava/CollectionsJavaSet.java
@@ -9,45 +9,45 @@ import java.util.*;
  */
 public class MainClasse {
     public static void main(String[] args) {
-        //Crie um conjunto e adicione as notas:
-        Set<Double> notas = new HashSet<>(Arrays.asList(7d,8.5,9.3,5d,7d,0d,3.6));
-        System.out.println(notas);//Neste caso o 7d que se repete só vai aparecer uma vez;
+        //Crie um conjunto e adicione as notes:
+        Set<Double> notes = new HashSet<>(Arrays.asList(7d,8.5,9.3,5d,7d,0d,3.6));
+        System.out.println(notes);//Neste caso o 7d que se repete só vai aparecer uma vez;
         //Confira se a nota 5d está no conjunto;
-        System.out.println(notas.contains(5d));
+        System.out.println(notes.contains(5d));
         //Exibir a menor nota:
-        System.out.println(Collections.min(notas));
+        System.out.println(Collections.min(notes));
         //Exibir a maior nota:
-        System.out.println(Collections.max(notas));
-        //Exibir a soma dos valores
-        Iterator<Double> iterator = notas.iterator();
-        Double soma = 0d;
+        System.out.println(Collections.max(notes));
+        //Exibir a sum dos valores
+        Iterator<Double> iterator = notes.iterator();
+        Double sum = 0d;
         while (iterator.hasNext()){
             Double next = iterator.next();
-            soma += next;
+            sum += next;
         }
-        System.out.println(soma);
-        //Exibir a média das notas
-        System.out.println((soma/notas.size()));
+        System.out.println(sum);
+        //Exibir a média das notes
+        System.out.println((sum/notes.size()));
         //Remover a nota 0.0:
-        notas.remove(0d);
-        System.out.println(notas);
-        //Remover as notas menores que 7d:
-        Iterator<Double> iterator1 = notas.iterator();
+        notes.remove(0d);
+        System.out.println(notes);
+        //Remover as notes menores que 7d:
+        Iterator<Double> iterator1 = notes.iterator();
         while (iterator1.hasNext()){
             Double next = iterator1.next();
             if (next < 7.0) iterator1.remove();
         }
-        System.out.println(notas);
-        //Exibir notas na ordem que foram inseridas:
-        Set<Double> notas2 = new LinkedHashSet<>(Arrays.asList(7d,8.5,9.3,5d,7d,0d,3.6));
-        System.out.println(notas2);
-        //Exibir todas as notas em ordem crescente:
-        Set<Double> notas3 = new TreeSet<>(notas2);
-        System.out.println(notas3);
+        System.out.println(notes);
+        //Exibir notes na ordem que foram inseridas:
+        Set<Double> notes2 = new LinkedHashSet<>(Arrays.asList(7d,8.5,9.3,5d,7d,0d,3.6));
+        System.out.println(notes2);
+        //Exibir todas as notes em ordem crescente:
+        Set<Double> notes3 = new TreeSet<>(notes2);
+        System.out.println(notes3);
         //Apagar todoo o conjunto;
-        notas.clear();
-        System.out.println(notas);
+        notes.clear();
+        System.out.println(notes);
         //Conferir se o conjunto está vazio:
-        System.out.println(notas.isEmpty());
+        System.out.println(notes.isEmpty());
     }
 }
