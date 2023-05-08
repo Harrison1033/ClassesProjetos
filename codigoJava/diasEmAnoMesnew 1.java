d77ccef9ed9a5a2f5927afe1572643def459cdfc
Harrison de Oliveira Rodrigues <95246993+Harrison1033@users.noreply.github.com>
2023-05-08 14:30:09 -0300

    Modificação de classe

--- codigoJava/diasEmAnoMesnew 1.java
+++ codigoJava/diasEmAnoMesnew 1.java
@@ -1,19 +1,19 @@
 package br.com.harrison.principal;
 
 import java.util.Scanner;
-//convertendo dias em anos, meses e dias
+//convertendo days em years, monthes e days
 public class TesteMain01 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Digite a idade em dias:");
-        int dia = scan.nextInt();
-        int ano = dia / 365;
-        dia %= 365;
-        int mes = dia / 30;
-        dia %= 30;
-        int dias = dia;
+        System.out.println("Enter age in days:");
+        int day = scan.nextInt();
+        int year = day / 365;
+        day %= 365;
+        int month = day / 30;
+        day %= 30;
+        int days = day;
 
-        System.out.println(ano + " Ano(s)\n" + mes + " Mes(es)\n" + dias + " Dia(s)");
+        System.out.println(year + " Year(s)\n" + month + " Month(es)\n" + days + " day(s)");
 
     }
 }
