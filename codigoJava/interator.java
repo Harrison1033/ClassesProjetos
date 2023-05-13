743cb5d0f07998af7722b52415de8b94430de6f4
Harrison de Oliveira Rodrigues <95246993+Harrison1033@users.noreply.github.com>
2023-05-13 19:19:10 -0300

    Modificação da classe

--- codigoJava/interator.java
+++ codigoJava/interator.java
@@ -8,22 +8,22 @@ public class TesteTres{
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         // vamos criar uma ArrayList
-        ArrayList<String> pessoas = new ArrayList();
+        ArrayList<String> people = new ArrayList();
 		//vamos digitar o numero de interações
-        System.out.println("Digite o número de nomes a serem digitados: ");
+        System.out.println("Enter the number of names to be entered: ");
         int vez = scan.nextInt();
         //vamos digitar os nomes pelo número de interações
         for (int i = 0; i < vez; i ++) {
-            System.out.println("Digite um nome: ");
-            pessoas.add(scan.next());
+            System.out.println("Enter a name: ");
+            people.add(scan.next());
         }
         // vamos obter um iterador para a lista
-        Iterator<String> pessoa = pessoas.iterator();
+        Iterator<String> person = people.iterator();
 
         // vamos obter o próximo elemento (neste caso, do primeiro ao quarto)
         for (int i = 0; i < vez; i ++) {
-            String nome = pessoa.next();
-            System.out.println("Nome " + (i +1) + " : " + nome);
+            String nome = person.next();
+            System.out.println("Name " + (i +1) + " : " + nome);
         }
     }
 }
