ca1dbed4cff1887a31243877cb2ec27b9343b6d7
Harrison de Oliveira Rodrigues <95246993+Harrison1033@users.noreply.github.com>
2023-05-14 11:37:46 -0300

    Modificação de classe

--- codigoJava/jurosSimples.java
+++ codigoJava/jurosSimples.java
@@ -3,15 +3,15 @@ package br.com.harrison.classe;
 import java.util.Scanner;
 public class TesteDois {
     Scanner scan = new Scanner(System.in);
-    double salrio;
-    double valorTotal;
+    double wage;
+    double amount;
 
     public void imprimir(){
-        System.out.println("Digite o valor do salário: ");
-        salrio = scan.nextDouble();
-        valorTotal = salrio + (salrio * 0.1);
-        System.out.println("O valor digitado é: " + salrio);
-        System.out.println("Com o aumento de 10% ficará: " + valorTotal);
+        System.out.println("Enter salary amount: ");
+        wage = scan.nextDouble();
+        amount = wage + (wage * 0.1);
+        System.out.println("The entered value is: " + wage);
+        System.out.println("With a 10% increase it will be: " + amount);
 
     }
 }
