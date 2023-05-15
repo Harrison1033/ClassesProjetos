1bf757fb6ac73ab96bef6af4ccb820dc77752238
Harrison de Oliveira Rodrigues <95246993+Harrison1033@users.noreply.github.com>
2023-05-15 09:20:10 -0300

    Modificação de classe

--- codigoJava/maiorDeTres.java
+++ codigoJava/maiorDeTres.java
@@ -5,19 +5,19 @@ import java.util.Scanner;
 public class TesteMain01 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Digite o primeiro número: ");
+        System.out.println("Enter the first number: ");
         int a = scan.nextInt();
-        System.out.println("Digite o segundo número: ");
+        System.out.println("Enter the second number: ");
         int b = scan.nextInt();
-        System.out.println("Digite o terceiro número: ");
+        System.out.println("Enter the third number: ");
         int c = scan.nextInt();
 
         if(a > b && a > c){
-            System.out.println(a + " eh o maior");
+            System.out.println(a + " it is bigger");
         }else if(b > c){
-            System.out.println(b + " eh o maior");
+            System.out.println(b + " it id bigger");
         }else {
-            System.out.println(c + " eh o maior");
+            System.out.println(c + " it is bigger");
         }
     }
 }
