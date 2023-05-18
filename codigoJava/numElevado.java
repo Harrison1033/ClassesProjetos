265b180dfc56331f98c80797637a264023588e2f
Harrison de Oliveira Rodrigues <95246993+Harrison1033@users.noreply.github.com>
2023-05-18 09:21:02 -0300

    Modificação de classe

--- codigoJava/numElevado.java
+++ codigoJava/numElevado.java
@@ -7,7 +7,7 @@ public class TesteMain01 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         DecimalFormat df = new DecimalFormat("0.000");
-        System.out.println("Digite o valor do raio: ");
+        System.out.println("Enter the radius value: ");
         double r = scan.nextDouble();
         double pi = 3.14159;
         double volume = ((4/3.0)* pi * Math.pow(r,3) );
