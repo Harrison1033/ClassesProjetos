835f32353af1419d564c7761daa968eecf5a860a
Harrison de Oliveira Rodrigues <95246993+Harrison1033@users.noreply.github.com>
2023-05-20 21:30:40 -0300

    Modificação de classe

--- codigoJava/raizQuadradaPoten.java
+++ codigoJava/raizQuadradaPoten.java
@@ -5,13 +5,13 @@ import java.util.Scanner;
 public class TesteMain01 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println( "Digite o valor de x1: ");
+        System.out.println( "Enter the value of x1: ");
         double x1 = scan.nextDouble();
-        System.out.println("Digite o valor de y1: ");
+        System.out.println("Enter the value of y1: ");
         double y1 = scan.nextDouble();
-        System.out.println("Digite o valor de x2: ");
+        System.out.println("Enter the value of x2: ");
         double x2 = scan.nextDouble();
-        System.out.println("Digite o valor de y2");
+        System.out.println("Enter the value of y2");
         double y2 = scan.nextDouble();
         double dist = Math.sqrt(Math.pow((x2 - x1), 2)+ Math.pow((y2 - y1), 2));
         System.out.printf("%.4f%n", dist);
