4a0cb85715a266fa178dbb0df761ade90ce8b1c9
Harrison de Oliveira Rodrigues <95246993+Harrison1033@users.noreply.github.com>
2023-05-22 09:43:54 -0300

    Modificação de classe

--- codigoJava/sobrecargaConstrutor.java
+++ codigoJava/sobrecargaConstrutor.java
@@ -1,25 +1,25 @@
 package br.com.harrison.classe;
 
 public class TesteUm {
-    private final String nome;
-    private final int idade;
-    private final char sexo;
-    private double altura;
-    public TesteUm(String nome, int idade, char sexo) {
-        this.nome = nome;
-        this.idade = idade;
-        this.sexo = sexo;
+    private final String name;
+    private final int age;
+    private final char sex;
+    private double height;
+    public TesteUm(String name, int age, char sex) {
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
     }
-    public TesteUm(String nome, int idade, char sexo, double altura) {
-        this(nome, idade, sexo ) ;
-        this.altura = altura;
+    public TesteUm(String name, int age, char sex, double height) {
+        this(name, age, sex ) ;
+        this.height = height;
     }
     public void imprimir(){
         System.out.println("_____________________");
-        System.out.println("Nome: " + this.nome);
-        System.out.println("Idade: "+ this.idade);
-        System.out.println("Sexo: " + this.sexo);
-        System.out.println("Altura: " + this.altura);
+        System.out.println("Name: " + this.name);
+        System.out.println("Age: "+ this.age);
+        System.out.println("Sex: " + this.sex);
+        System.out.println("Height: " + this.height);
     }
 }
 
@@ -28,10 +28,10 @@ package br.com.harrison.principal;
 
 import br.com.harrison.classe.TesteUm;
 
-public class TesteMain01 {
+public class Main {
     public static void main(String[] args) {
-        TesteUm testeUm = new TesteUm("Harrison de Oliveira", 42, 'M',1.82);
-        TesteUm testeUm1 = new TesteUm("Keliane de Lima", 37, 'F', 1.60);
+        TesteUm testeUm = new TesteUm("Oligariato Teixeira", 42, 'M',1.82);
+        TesteUm testeUm1 = new TesteUm("Ana Letícia Tícia", 37, 'F', 1.60);
         testeUm.imprimir();
         testeUm1.imprimir();
 
