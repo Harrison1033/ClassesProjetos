0c1de4d0d5cbfb95be9f1d3c5cc7d838ff99ca0c
Harrison de Oliveira Rodrigues <95246993+Harrison1033@users.noreply.github.com>
2023-05-30 13:56:47 -0300

    Modificação do método Main de Tabuada

--- codigoJava/Tabuada/principal.java
+++ codigoJava/Tabuada/principal.java
@@ -1,37 +1,37 @@
 package bk.com.harrison.principal;
 
-import bk.com.harrison.classes.ClasseA1;
-import bk.com.harrison.classes.ClasseA2;
-import bk.com.harrison.classes.ClasseA3;
-import bk.com.harrison.classes.ClasseA4;
+import bk.com.harrison.classes.Sum;
+import bk.com.harrison.classes.Subtraction;
+import bk.com.harrison.classes.Multiplication;
+import bk.com.harrison.classes.Division;
 
 import java.util.Scanner;
 
-public class Principal {
+public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        ClasseA1 classeA1 = new ClasseA1();
-        ClasseA2 classeA2 = new ClasseA2();
-        ClasseA3 classeA3 = new ClasseA3();
-        ClasseA4 classeA4 = new ClasseA4();
+        Sum sum = new Sum();
+        Subtraction subtraction = new Subtraction();
+        Multiplication multiplication = new Multiplication();
+        Division division = new Division();
         int opc;
-        String opc2 = "s";
+        String opc2 = "y";
 
-        while ("s".equals(opc2)) {
-            System.out.println("Digite a operação desejada: \n(1) Soma\n(2) Subtração\n(3) Multiplicação\n(4) Divisão");
+        while ("y".equals(opc2)) {
+            System.out.println("Enter the desired operation: \n(1) Sum\n(2) Subtraction\n(3) Multiplication\n(4) Division");
             opc = scan.nextInt();
             if (opc == 1) {
-                classeA1.imprimir();
+                sum.imprimir();
             } else if (opc == 2) {
-                classeA2.imprimir();
+                subtraction.imprimir();
             } else if (opc == 3) {
-                classeA3.imprimir();
+                multiplication.imprimir();
             } else if (opc == 4) {
-                classeA4.imprimir();
+                division.imprimir();
             } else {
-                System.out.println("Opção inválida: ");
+                System.out.println("Invalid option: ");
             }
-            System.out.println("Gostaris de continuar? \n(s) Sim\n(n) Não");
+            System.out.println("Would you like to continue? \n(y) Yes\n(n) No");
             opc2 = scan.next();
         }
     }
