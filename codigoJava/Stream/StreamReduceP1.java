afa0b2f10885ef6c229982c5d3723ed9f69e02ad
Harrison de Oliveira Rodrigues <95246993+Harrison1033@users.noreply.github.com>
2023-06-01 13:32:56 -0300

    Modificação da classe StreamReduceP1

--- codigoJava/Stream/StreamReduceP1.java
+++ codigoJava/Stream/StreamReduceP1.java
@@ -6,15 +6,15 @@ import java.util.List;
 public class MainUm {
     public static void main(String[] args) {
         List<Integer>integers = List.of(1,2,3,4,5,6);
-        System.out.println("-------------Somando---------------");
+        System.out.println("-------------Adding---------------");
         integers.stream().reduce((x,y)-> x + y).ifPresent(System.out::println);//as 4 opcs fazem o mesmo
         System.out.println(integers.stream().reduce(0,(x,y)->x + y));
         integers.stream().reduce(Integer::sum).ifPresent(System.out::println);
         System.out.println(integers.stream().reduce(0,Integer::sum));
-        System.out.println("-----------Multiplicando-----------");
+        System.out.println("-----------Multiplying----------------------");
         integers.stream().reduce((x,y)->x * y).ifPresent(System.out::println);//as 2 opcs fazem o mesmo
         System.out.println(integers.stream().reduce(1,(x,y)->x * y));
-        System.out.println("------------Maior Valor------------");
+        System.out.println("------------Highest Value------------");
         integers.stream().reduce((x,y)->x > y ? x : y).ifPresent(System.out::println);//as 2 opcs fazem o mesmo
         System.out.println(integers.stream().reduce(0,Integer::max));
     }
