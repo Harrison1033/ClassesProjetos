af386a28aabc93f171eba46050e99de418beb2b0
Harrison de Oliveira Rodrigues <95246993+Harrison1033@users.noreply.github.com>
2023-06-03 20:55:26 -0300

    Modificação da ClasseA

--- codigoJava/projetoMercado2/ClasseAa.java
+++ codigoJava/projetoMercado2/ClasseAa.java
@@ -5,25 +5,25 @@ import java.util.Scanner;
 public class ClasseAa {//soma de valores e quantidade de produtos
 	Scanner scan = new Scanner(System.in);
 	protected int quantidade, opcao;
-	protected double valor1;
-	protected double valor2 = 0;
-	protected double resultado;
+	protected double value1;
+	protected double value2 = 0;
+	protected double result;
 	
-	public void imprimir() {
+	public void printOut() {
 		while (true) {
-			System.out.println("digite o valor do produto");
-			valor1 = scan.nextDouble();
-			System.out.println("digite a quantidade de produtos");
+			System.out.println("Enter the product value");
+			value1 = scan.nextDouble();
+			System.out.println("Enter the quantity of products");
 			quantidade = scan.nextInt();
-			valor2 = valor1 * quantidade;
-			resultado += valor2;
-			System.out.println("Gostaria de cadastrar outro produto?\n(1) Sim\n(2) Não");
+			value2 = value1 * quantidade;
+			result += value2;
+			System.out.println("Would you like to register another product?\n(1) Yes\n(2) No");
 			opcao = scan.nextInt();
 			if (opcao == 2) {
 				break;
 			}
 		}
-		System.out.printf("O valor total é: %.2f " , resultado);
+		System.out.printf("The total amount is: %.2f " , result);
 		
 	}
 
