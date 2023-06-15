929d98c8033589d42325974c48a5ab6eafa61602
Harrison de Oliveira Rodrigues <95246993+Harrison1033@users.noreply.github.com>
2023-06-15 14:13:29 -0300

    Modificação da classe Aplicação2

--- codigoJava/Ordenação/OrdenacaoEmColecaoStream/Aplicacao2.java
+++ codigoJava/Ordenação/OrdenacaoEmColecaoStream/Aplicacao2.java
@@ -7,41 +7,41 @@ import java.util.stream.Collectors;
 
 public class MainClasse2 {
     public static void main(String[] args) {
-        List<String> numerosAleatorios = Arrays.asList("1","0","4","1","2","3","9","9","6","5");
-        System.out.println("imprima todos os elementos da lista: ");
-//        numerosAleatorios.stream().forEach(new Consumer<String>() {
+        List<String> randomNumbers = Arrays.asList("1","0","4","1","2","3","9","9","6","5");
+        System.out.println("print all list elements: ");
+//        randomNumbers.stream().forEach(new Consumer<String>() {
 //            @Override
 //            public void accept(String s) {
 //                System.out.println(s);
 //            }
 //        });
-        numerosAleatorios.forEach(System.out::println);//esse código faz o mesmo que o código acima
+        randomNumbers.forEach(System.out::println);//esse código faz o mesmo que o código acima
 
-        System.out.println("Pegue os 5 primeiros número e coloque no set");
-        numerosAleatorios.stream()
+        System.out.println("Take the first 5 numbers and put them in the set");
+        randomNumbers.stream()
                 .limit(5)
                 .collect(Collectors.toSet())//O set não aceita números repetidos
                 .forEach(System.out::println);
 
-        System.out.println("Transforme a lista de strings em uma lista de inteiros");
-        numerosAleatorios.stream()
+        System.out.println("Turn the list of strings into a list of integers");
+        randomNumbers.stream()
                 .map(Integer::parseInt)
                 .collect(Collectors.toList())
                 .forEach(System.out::println);
-   System.out.println("Numeros pares e maiores que dois: ");
-        numerosAleatorios.stream()
+   System.out.println("Even numbers greater than two: ");
+        randomNumbers.stream()
                 .map(Integer::parseInt)
                 .filter(i -> (i % 2 == 0 && i > 2)).collect(Collectors.toList())
                 .forEach(System.out::println);
 
-        System.out.println("Mostrar a média dos números: ");
-        numerosAleatorios.stream()
+        System.out.println("Show the average of the numbers: ");
+        randomNumbers.stream()
                 .mapToInt(Integer::parseInt)
                 .average()
                 .ifPresent(System.out::println);
 
-        System.out.println("Remove os valores ímpares: ");
-        List<Integer>numAleatorioInteger = numerosAleatorios.stream()
+        System.out.println("Remove odd values: ");
+        List<Integer>numAleatorioInteger = randomNumbers.stream()
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
 
