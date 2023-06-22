49243886279ddb3c549d9d5b6f1ca4e9a99179c1
Harrison de Oliveira Rodrigues <95246993+Harrison1033@users.noreply.github.com>
2023-06-22 13:22:45 -0300

    Refatoração de Classe

--- codigoJava/Ordenação/OrdenacaoEmColecaoMap/Aplicacao.java
+++ codigoJava/Ordenação/OrdenacaoEmColecaoMap/Aplicacao.java
@@ -8,33 +8,33 @@ import java.util.*;
 public class MainClasse2 {
     public static void main(String[] args) {
         //ORDEM ALEATÓRIA
-        Map<String, PrimeiraClasse> meusLivros = new HashMap<>(){{
+        Map<String, PrimeiraClasse> myBooks = new HashMap<>(){{
             put("Hawking, Stephen", new PrimeiraClasse("Uma Breve História do Tempo",256));
             put("Duhigg, Charles", new PrimeiraClasse("O Poder do Hábito", 408));
             put("Harari, Yuval Noah", new PrimeiraClasse("21 Lições para o século 21", 432));
         }};
-        for (Map.Entry<String,PrimeiraClasse> livro: meusLivros.entrySet()) {
-            System.out.println(livro.getKey() + "-" + livro.getValue().getNome());
+        for (Map.Entry<String,PrimeiraClasse> book: myBooks.entrySet()) {
+            System.out.println(book.getKey() + "-" + book.getValue().getName());
         }
         //ORDEM ALFABÉTICA DE INSERÇÃO
-        Map<String, PrimeiraClasse> meusLivros1 = new LinkedHashMap<>(){{
+        Map<String, PrimeiraClasse> myBooks1 = new LinkedHashMap<>(){{
             put("Hawking, Stephen", new PrimeiraClasse("Uma Breve História do Tempo",256));
             put("Duhigg, Charles", new PrimeiraClasse("O Poder do Hábito", 408));
             put("Harari, Yuval Noah", new PrimeiraClasse("21 Lições para o século 21", 432));
         }};
-        for (Map.Entry<String,PrimeiraClasse> livro: meusLivros1.entrySet()) {
-            System.out.println(livro.getKey() + "-" + livro.getValue().getNome());
+        for (Map.Entry<String,PrimeiraClasse> book: myBooks1.entrySet()) {
+            System.out.println(book.getKey() + "-" + book.getValue().getName());
         }
         //ORDEM ALFABÉTICA DOS AUTORES(CHAVE)
-        Map<String, PrimeiraClasse> meusLivros2 = new TreeMap<>(meusLivros1);
-        for (Map.Entry<String,PrimeiraClasse> livro: meusLivros2.entrySet()) {
-            System.out.println(livro.getKey() + "-" + livro.getValue().getNome());
+        Map<String, PrimeiraClasse> myBooks2 = new TreeMap<>(myBooks1);
+        for (Map.Entry<String,PrimeiraClasse> book: myBooks2.entrySet()) {
+            System.out.println(book.getKey() + "-" + book.getValue().getName());
         }
-        //ORDEM ALFABÉTICA NOME DOS LIVOS(Nome dos livros é um valor e por isso usamos o comparator)
-        Set<Map.Entry<String, PrimeiraClasse>>meusLivros3 = new TreeSet<>(new SegundaClasse());
-        meusLivros3.addAll(meusLivros.entrySet());
-        for (Map.Entry<String, PrimeiraClasse>livro : meusLivros3){
-            System.out.println(livro.getKey()+ "-" + livro.getValue().getNome());
+        //ORDEM ALFABÉTICA NOME DOS LIVOS(Nome dos books é um valor e por isso usamos o comparator)
+        Set<Map.Entry<String, PrimeiraClasse>>myBooks3 = new TreeSet<>(new SegundaClasse());
+        myBooks3.addAll(myBooks.entrySet());
+        for (Map.Entry<String, PrimeiraClasse>book : myBooks3){
+            System.out.println(book.getKey()+ "-" + book.getValue().getName());
         }
 //ORDEM POR NÚMERO DE PÁGINAS
 
