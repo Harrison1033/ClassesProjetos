d00df7a99fa27c9f946514ba33eaafcc1d9cdfb9
Harrison de Oliveira Rodrigues <95246993+Harrison1033@users.noreply.github.com>
2023-06-23 12:46:32 -0300

    Modificação da classe NomeCorIdade

--- codigoJava/Ordenação/OrdenacaoEmColecaoList/NomeCorIdade.java
+++ codigoJava/Ordenação/OrdenacaoEmColecaoList/NomeCorIdade.java
@@ -5,10 +5,10 @@ import java.util.Comparator;
 public class QuartaClasse implements Comparator<TerceiraClasse> {
     @Override
     public int compare(TerceiraClasse g1, TerceiraClasse g2) {
-        int nome = g1.getNome().compareToIgnoreCase(g2.getNome());
-        if (nome != 0) return nome;
-        int cor = g1.getCor().compareToIgnoreCase(g2.getCor());
-        if (cor != 0) return cor;
-        return Integer.compare(g1.getIdade(),g2.getIdade());
+        int name = g1.getName().compareToIgnoreCase(g2.getName());
+        if (name != 0) return name;
+        int color = g1.getColor().compareToIgnoreCase(g2.getColor());
+        if (color != 0) return color;
+        return Integer.compare(g1.getAge(),g2.getAge());
     }
 }
