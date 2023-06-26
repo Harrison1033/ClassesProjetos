3eb3d455e68cb6e76dbe756e62388df866c1bbad
Harrison de Oliveira Rodrigues <95246993+Harrison1033@users.noreply.github.com>
2023-06-26 09:15:41 -0300

    Refatoração da classe Cor

--- codigoJava/Ordenação/OrdenacaoEmColecaoList/Cor.java
+++ codigoJava/Ordenação/OrdenacaoEmColecaoList/Cor.java
@@ -6,6 +6,6 @@ public class PrimeiraClasse implements Comparator<TerceiraClasse> {
 
     @Override
     public int compare(TerceiraClasse g1, TerceiraClasse g2) {
-        return g1.getCor().compareToIgnoreCase(g2.getCor());
+        return g1.getColor().compareToIgnoreCase(g2.getColor());
     }
 }
