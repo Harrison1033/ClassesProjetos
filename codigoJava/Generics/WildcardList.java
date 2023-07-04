032bf812d66809d30f6a42f3fa561e55f2efba44
Harrison de Oliveira Rodrigues <95246993+Harrison1033@users.noreply.github.com>
2023-07-04 15:09:45 -0300

    Refatoração da classe WildcardList

--- codigoJava/Generics/WildcardList.java
+++ codigoJava/Generics/WildcardList.java
@@ -5,15 +5,15 @@ import java.util.List;
 //WildcardList
 public class ClasseUm {
     public static void main(String[] args) {
-        List<Cachorro> cachorros = List.of(new Cachorro(), new Cachorro());
-        List<Gato> gatos = List.of(new Gato(), new Gato());
-        printConsulta(cachorros);
-        printConsulta(gatos);
+        List<Dog> dogs = List.of(new Dog(), new Dog());
+        List<Cat> cats = List.of(new Cat(), new Cat());
+        printConsulta(dogs);
+        printConsulta(cats);
     }
 
     private static void printConsulta(List<? extends Animal> animals) {//Para leitura(Não permite adicionar)
         for (Animal animal : animals) {
-            animal.consultar();
+            animal.consult();
         }
     }
 }
