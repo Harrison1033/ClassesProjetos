f56373ee9f194427016247ca85b9c91c24dc9eb8
Harrison de Oliveira Rodrigues <95246993+Harrison1033@users.noreply.github.com>
2023-07-06 11:41:13 -0300

    Refatoração de MetodosGenericos

--- codigoJava/Generics/MetodosGenericos.java
+++ codigoJava/Generics/MetodosGenericos.java
@@ -5,23 +5,23 @@ import java.util.List;
 //Métodos Genéricos
 public class ClasseQuatro {
     public static void main(String[] args) {
-        List<Carro> carroList = criandoArryComObjeto(new Carro("fusca"));
-        System.out.println(carroList);
+        List<Car> carList = criandoArryComObjeto(new Car("fusca"));
+        System.out.println(carList);
     }
 
     private static <T> List<T> criandoArryComObjeto(T t) {
         return List.of(t);
     }
-    public static class Carro {
-        private final String nome;
+    public static class Car {
+        private final String name;
 
-        public Carro(String nome) {
-            this.nome = nome;
+        public Car(String name) {
+            this.name = name;
         }
 
         @Override
         public String toString() {
-            return "Carro{" + "nome='" + nome + '\'' + '}';
+            return "Car{" + "name='" + name + '\'' + '}';
         }
     }
 }
