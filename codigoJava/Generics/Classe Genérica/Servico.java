0bf83e33b7a6aba5d7426c8e8338d68c4e30c8fe
Harrison de Oliveira Rodrigues <95246993+Harrison1033@users.noreply.github.com>
2023-07-08 18:56:51 -0300

    Modificação da classe Servico de Generic

--- codigoJava/Generics/Classe Genérica/Servico.java
+++ codigoJava/Generics/Classe Genérica/Servico.java
@@ -3,23 +3,23 @@ package teste;
 import java.util.List;
 
 public class ClasseTres<T> {//O "T" representa o tipo(Type)...
-    private List<T>objetosDisponiveis;
+    private List<T>avaliableObjects;
 
-    public ClasseTres(List<T> objetosDisponiveis) {
-        this.objetosDisponiveis = objetosDisponiveis;
+    public ClasseTres(List<T> avaliableObjects) {
+        this.avaliableObjects = avaliableObjects;
     }
     public T buscandoObjetoDisponivel(){
         System.out.println("Buscando Objeto disponível: ");
-        T t = objetosDisponiveis.remove(0);
+        T t = avaliableObjects.remove(0);
         System.out.println("Alugando Objeto: " + t);
         System.out.println("Objetos disponíveis para alugar");
-        System.out.println(objetosDisponiveis);
+        System.out.println(avaliableObjects);
         return t;
     }
     public void retornaObjetoAlugado(T t){
         System.out.println("Devlovendo Objeto " + t);
-        objetosDisponiveis.add(t);
+        avaliableObjects.add(t);
         System.out.println("Objetos disponíveis para alugar: ");
-        System.out.println(objetosDisponiveis);
+        System.out.println(avaliableObjects);
     }
 }
