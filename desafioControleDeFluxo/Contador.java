4b24cb74a79989808bb29ea96246f3ba40e90959
Harrison de Oliveira Rodrigues <95246993+Harrison1033@users.noreply.github.com>
2023-07-20 16:30:00 -0300

    Criação da classe principal

---
+++ desafioControleDeFluxo/Contador.java
@@ -0,0 +1,11 @@
+package teste;
+
+public class Contador {
+    public static void main(String[] args) throws Exception {
+     ParametrosInvalidosException parametrosInvalidosException = new ParametrosInvalidosException();
+
+          parametrosInvalidosException();
+
+
+    }
+}
