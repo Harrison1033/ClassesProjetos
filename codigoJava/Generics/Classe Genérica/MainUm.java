8976cee70575445cc8750db8183096b398c06fb2
Harrison de Oliveira Rodrigues <95246993+Harrison1033@users.noreply.github.com>
2023-07-25 14:35:43 -0300

    Comentário 1

--- codigoJava/Generics/Classe Genérica/MainUm.java
+++ codigoJava/Generics/Classe Genérica/MainUm.java
@@ -7,7 +7,7 @@ public class MainUm {
     public static void main(String[] args) {
         List<ClasseUm>carsAvailable = new ArrayList<>(List.of(new ClasseUm("BMW"),new ClasseUm("Fusca")));
         List<ClasseDois>barcosDisponiveis = new ArrayList<>(List.of(new ClasseDois("Lancha"),new ClasseDois("Canoa")));
-
+		//for car
         ClasseTres<ClasseUm>umClasseTres = new ClasseTres<>(carsAvailable);
         ClasseUm car = umClasseTres.buscandoObjetoDisponivel();
         System.out.println("Usingi the car for a month...");
