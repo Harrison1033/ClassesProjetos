3975ef4b4edbdbf3d5eec5cebd5db8768e2a84c5
Harrison de Oliveira Rodrigues <95246993+Harrison1033@users.noreply.github.com>
2023-07-27 08:33:05 -0300

    Melhoria da classe Main do projeto diagrama de classes do iPhone

--- Diagramacao de Classes do iPhone/Main.java
+++ Diagramacao de Classes do iPhone/Main.java
@@ -1,22 +1,31 @@
 package teste;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+        System.out.println("Escolha uma das seguintes opções: ");
+        System.out.println("1-Reprodutor Musical\n2-Aparelho Telefonico\n3-Navegador na Internet");
+        int num = scan.nextInt();
         IPhone iPhone = new IPhone();
 		
+		if (num == 1) {
         System.out.println("-------Reprodutor Musical:-------");
         iPhone.tocar();
         iPhone.pausar();
         iPhone.selecionarMusica();
-		
+		} else if (num == 2) {
         System.out.println("-------Aparelho telefonico:-------");
         iPhone.ligar();
         iPhone.atender();
         iPhone.iniciarCorreioDeVoz();
-		
+		} else if (num == 3) {
         System.out.println("-------Navegador na Internet:-------");
         iPhone.exibirPagina();
         iPhone.adicionarNovaAba();
         iPhone.atualizarPagina();
+		  } else
+            System.out.println("Opção Inválida:");
     }
 }
