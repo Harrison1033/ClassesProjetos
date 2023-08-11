4981f18dc4bdb500c95df90cecee7a4638d48616
Harrison de Oliveira Rodrigues <95246993+Harrison1033@users.noreply.github.com>
2023-08-11 12:51:50 -0300

    Comentários adicionados á classe Item

--- codigoJava/CollectionsJava/ListaCompra/Item.java
+++ codigoJava/CollectionsJava/ListaCompra/Item.java
@@ -4,13 +4,13 @@ public class Item <T>{
    private String nome;
    private double preco;
    private int quantidade;
-
+	//Construtor
     public Item(String nome, double preco, int quantidade) {
         this.nome = nome;
         this.preco = preco;
         this.quantidade = quantidade;
     }
-
+	//Getters
     public String getNome() {
         return nome;
     }
@@ -22,7 +22,7 @@ public class Item <T>{
     public int getQuantidade() {
         return quantidade;
     }
-
+	//toString
     @Override
     public String toString() {
         return "Item{" +
