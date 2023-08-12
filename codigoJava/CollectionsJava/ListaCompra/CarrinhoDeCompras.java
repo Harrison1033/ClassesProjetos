35f906397221bb18235307e25dd34e0153f3b403
Harrison de Oliveira Rodrigues <95246993+Harrison1033@users.noreply.github.com>
2023-08-12 17:17:23 -0300

    Criação da classe CarrinhoDeCompras

---
+++ codigoJava/CollectionsJava/ListaCompra/CarrinhoDeCompras.java
@@ -0,0 +1,19 @@
+package teste;
+
+public class Main {
+    public static void main(String[] args) {
+        CarrinhoDeCompras carrinhoDeCompras = new CarrinhoDeCompras();
+
+        carrinhoDeCompras.adicionarItem("Lápis", 2d, 3);
+        carrinhoDeCompras.adicionarItem("Lápis", 2d, 3);
+        carrinhoDeCompras.adicionarItem("Caderno", 35d, 1);
+        carrinhoDeCompras.adicionarItem("Borracha", 2d, 2);
+
+        carrinhoDeCompras.exibirItens();
+
+        carrinhoDeCompras.removerItem("Lápis");
+        carrinhoDeCompras.exibirItens();
+
+        System.out.println("O valor total da compra é = " + carrinhoDeCompras.calcularValorTotal());
+    }
+}
