d763d52a4897d350832837cf7ebb667731f97360
Harrison de Oliveira Rodrigues <95246993+Harrison1033@users.noreply.github.com>
2023-08-16 12:55:48 -0300

    Criação da Classe Main

---
+++ codigoJava/CollectionsJava/SetOrdenacao/Produto/Main.java
@@ -0,0 +1,20 @@
+package teste;
+
+//Main (Set)
+public class Main {
+    public static void main(String[] args) {
+        CadastroProduto cadastroProdutos = new CadastroProduto();
+
+        cadastroProdutos.adicionarProduto(1L, "Produto 5", 15d, 5);
+        cadastroProdutos.adicionarProduto(2L, "Produto 0", 20d, 10);
+        cadastroProdutos.adicionarProduto(1L, "Produto 3", 10d, 2);
+        cadastroProdutos.adicionarProduto(9L, "Produto 9", 2d, 2);
+
+        System.out.println(cadastroProdutos.produtoSet);
+
+        System.out.println(cadastroProdutos.exibirProdutosPorNome());
+
+        System.out.println(cadastroProdutos.exibirProdutosPorPreco());
+    }
+
+}
