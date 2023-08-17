ea90a9a8d1e2bc88689dd44bbca899dc727667c9
Harrison de Oliveira Rodrigues <95246993+Harrison1033@users.noreply.github.com>
2023-08-17 10:46:14 -0300

    Criação das classes para ordenação de números

---
+++ codigoJava/CollectionsJava/ListOrdenacao/Numeoro/Main.java
@@ -0,0 +1,24 @@
+package teste;
+
+//Main (List)
+public class Main {
+    public static void main(String[] args) {
+        OrdenacaoNumero numeros = new OrdenacaoNumero();
+
+        numeros.adicionarNumero(2);
+        numeros.adicionarNumero(5);
+        numeros.adicionarNumero(4);
+        numeros.adicionarNumero(1);
+        numeros.adicionarNumero(99);
+
+        numeros.exibirNumeros();
+
+        System.out.println(numeros.ordenarAscendente());
+
+        numeros.exibirNumeros();
+
+        System.out.println(numeros.ordenarDescendente());
+
+        numeros.exibirNumeros();
+    }
+}
---
+++ codigoJava/CollectionsJava/ListOrdenacao/Numeoro/OrdenacaoNumero.java
@@ -0,0 +1,48 @@
+package teste;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+//OrdenacaoNumero
+public class OrdenacaoNumero {
+    //atributos
+    private List<Integer> numerosList;
+
+    //construtor
+    public OrdenacaoNumero() {
+        this.numerosList = new ArrayList<>();
+    }
+
+    public void adicionarNumero(int numero) {
+        this.numerosList.add(numero);
+    }
+
+    public List<Integer> ordenarAscendente() {
+        List<Integer> numerosAscendente = new ArrayList<>(this.numerosList);
+        if (!numerosList.isEmpty()) {
+            Collections.sort(numerosAscendente);
+            return numerosAscendente;
+        } else {
+            throw new RuntimeException("A lista está vazia!");
+        }
+    }
+
+    public List<Integer> ordenarDescendente() {
+        List<Integer> numerosAscendente = new ArrayList<>(this.numerosList);
+        if (!numerosList.isEmpty()) {
+            numerosAscendente.sort(Collections.reverseOrder());
+            return numerosAscendente;
+        } else {
+            throw new RuntimeException("A lista está vazia!");
+        }
+    }
+
+    public void exibirNumeros() {
+        if (!numerosList.isEmpty()) {
+            System.out.println(this.numerosList);
+        } else {
+            System.out.println("A lista está vazia!");
+        }
+    }
+}
