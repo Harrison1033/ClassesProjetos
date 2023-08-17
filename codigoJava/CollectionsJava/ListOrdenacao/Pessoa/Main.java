12245ba966b320e3e8354ff1e5e07fac472ae330
Harrison de Oliveira Rodrigues <95246993+Harrison1033@users.noreply.github.com>
2023-08-17 10:26:16 -0300

    Criação da classe MainList

---
+++ codigoJava/CollectionsJava/ListOrdenacao/Pessoa/Main.java
@@ -0,0 +1,19 @@
+package teste;
+
+//Main (List)
+public class Main {
+    public static void main(String[] args) {
+        OrdenacaoPessoa ordenacaoPessoa = new OrdenacaoPessoa();
+
+        ordenacaoPessoa.adicionarPessoa("Nome 1", 20, 1.56);
+        ordenacaoPessoa.adicionarPessoa("Nome 2", 30, 1.80);
+        ordenacaoPessoa.adicionarPessoa("Nome 3", 25, 1.70);
+        ordenacaoPessoa.adicionarPessoa("Nome 4", 17, 1.56);
+
+        System.out.println(ordenacaoPessoa.pessoaList);
+
+        System.out.println(ordenacaoPessoa.ordenarPorIdade());
+
+        System.out.println(ordenacaoPessoa.ordenarPorAltura());
+    }
+}
