18621c740602b5eaa5f5c281c1b0fe510005f48c
Harrison de Oliveira Rodrigues <95246993+Harrison1033@users.noreply.github.com>
2023-08-17 09:58:25 -0300

    Criação da classe MainSet

---
+++ codigoJava/CollectionsJava/SetPesquisa/Contato/Main.java
@@ -0,0 +1,25 @@
+package teste;
+
+//Main (Set)
+public class Main {
+    public static void main(String[] args) {
+        AgnedaContatos agendaContatos = new AgnedaContatos();
+
+        agendaContatos.exibirContatos();
+
+        agendaContatos.adicionarContato("Camila", 123456);
+        agendaContatos.adicionarContato("Camila", 5665);
+        agendaContatos.adicionarContato("Camila", 5665);
+        agendaContatos.adicionarContato("Camila Cavalcante", 1111111);
+        agendaContatos.adicionarContato("Camila DIO", 654987);
+        agendaContatos.adicionarContato("Maria Silva", 1111111);
+
+        agendaContatos.exibirContatos();
+
+        System.out.println(agendaContatos.pesquisarPorNome("Camila"));
+
+        System.out.println("Contato atualizado: " + agendaContatos.atualizarNumeroContato("Maria Silva", 5555555));
+
+        agendaContatos.exibirContatos();
+    }
+}
