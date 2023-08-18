a837900925429f3af4794727bcb0a498dc0c611d
Harrison de Oliveira Rodrigues <95246993+Harrison1033@users.noreply.github.com>
2023-08-18 09:23:11 -0300

    Criação de classes em listpesquisa

---
+++ codigoJava/CollectionsJava/ListPesquisa/Livro/CatalogarLivro.java
@@ -0,0 +1,61 @@
+package teste;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//CatalogarLivro(List)
+public class CatalogarLivro {
+    //atributo
+    private List<Livro> livroList;
+
+    public CatalogarLivro() {
+        this.livroList = new ArrayList<>();
+    }
+
+    public void adicionarLivro(String titulo, String autor, int anoPublicacao) {
+        livroList.add(new Livro(titulo, autor, anoPublicacao));
+    }
+
+    public List<Livro> pesquisarPorAutor(String autor) {
+        List<Livro> livrosPorAutor = new ArrayList<>();
+        if (!livroList.isEmpty()) {
+            for (Livro l : livroList) {
+                if (l.getAutor().equalsIgnoreCase(autor)) {
+                    livrosPorAutor.add(l);
+                }
+            }
+            return livrosPorAutor;
+        } else {
+            throw new RuntimeException("A lista está vazia!");
+        }
+    }
+
+    public List<Livro> pesquisarPorIntervaloAnos(int anoInicial, int anoFinal) {
+        List<Livro> livrosPorIntervaloAnos = new ArrayList<>();
+        if (!livroList.isEmpty()) {
+            for (Livro l : livroList) {
+                if (l.getAnoPublicacao() >= anoInicial && l.getAnoPublicacao() <= anoFinal) {
+                    livrosPorIntervaloAnos.add(l);
+                }
+            }
+            return livrosPorIntervaloAnos;
+        } else {
+            throw new RuntimeException("A lista está vazia!");
+        }
+    }
+
+    public Livro pesquisarPorTitulo(String titulo) {
+       Livro livroPorTitulo = null;
+        if (!livroList.isEmpty()) {
+            for (Livro l : livroList) {
+                if (l.getTitulo().equalsIgnoreCase(titulo)) {
+                    livroPorTitulo = l;
+                    break;
+                }
+            }
+            return livroPorTitulo;
+        } else {
+            throw new RuntimeException("A lista está vazia!");
+        }
+    }
+}
