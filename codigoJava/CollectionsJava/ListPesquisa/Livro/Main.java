07174fb2987a24b26f34d65cd7f8bcebf89ad54d
Harrison de Oliveira Rodrigues <95246993+Harrison1033@users.noreply.github.com>
2023-08-18 09:25:44 -0300

    Criação da classe MainList

---
+++ codigoJava/CollectionsJava/ListPesquisa/Livro/Main.java
@@ -0,0 +1,20 @@
+package teste;
+
+//Main (List)
+public class Main {
+    public static void main(String[] args) {
+        CatalogarLivro catalogoLivros = new CatalogarLivro();
+
+        catalogoLivros.adicionarLivro("Livro 1", "Autor 1", 2021);
+        catalogoLivros.adicionarLivro("Livro 1", "Autor 2", 2020);
+        catalogoLivros.adicionarLivro("Livro 2", "Autor 2", 2022);
+        catalogoLivros.adicionarLivro("Livro 3", "Autor 3", 2023);
+        catalogoLivros.adicionarLivro("Livro 4", "Autor 4", 1994);
+
+        System.out.println(catalogoLivros.pesquisarPorAutor("Autor 2"));
+
+        System.out.println(catalogoLivros.pesquisarPorIntervaloAnos(2020, 2022));
+
+        System.out.println(catalogoLivros.pesquisarPorTitulo("Livro 1"));
+    }
+}
