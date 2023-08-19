df0286375befc2887ee1fa8933f5851ae094ba01
Harrison de Oliveira Rodrigues <95246993+Harrison1033@users.noreply.github.com>
2023-08-19 20:03:55 -0300

    Criação da classe SomaNumeros

---
+++ codigoJava/CollectionsJava/ListPesquisa/SomaNumeros/SomaNumeros.java
@@ -0,0 +1,62 @@
+package teste;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//SomaNumeros
+public class SomaNumeros {
+    //atributos
+    private List<Integer> numeros;
+
+    //construtor
+    public SomaNumeros() {
+        this.numeros = new ArrayList<>();
+    }
+
+    public void adicionarNumero(int numero) {
+        this.numeros.add(numero);
+    }
+
+    public int calcularSoma() {
+        int soma = 0;
+        for (Integer numero : numeros)
+            soma += numero;
+        return soma;
+    }
+
+    public int encontrarMaiorNumero() {
+        int maiorNumero = Integer.MIN_VALUE;
+        if (!numeros.isEmpty()) {
+            for (Integer numero : numeros) {
+                if (numero >= maiorNumero) {
+                    maiorNumero = numero;
+                }
+            }
+            return maiorNumero;
+        } else {
+            throw new RuntimeException("A lista está vazia!");
+        }
+    }
+
+    public int encontrarMenorNumero() {
+        int menorNumero = Integer.MAX_VALUE;
+        if (!numeros.isEmpty()) {
+            for (Integer numero : numeros) {
+                if (numero <= menorNumero) {
+                    menorNumero = numero;
+                }
+            }
+            return menorNumero;
+        } else {
+            throw new RuntimeException("A lista está vazia!");
+        }
+    }
+
+    public void exibirNumeros() {
+        if (!numeros.isEmpty()) {
+            System.out.println(this.numeros);
+        } else {
+            System.out.println("A lista está vazia!");
+        }
+    }
+}
