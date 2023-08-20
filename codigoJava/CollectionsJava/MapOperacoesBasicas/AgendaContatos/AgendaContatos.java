1ebe6aa9d93d78868d4e28576d18ee75d08c5e94
Harrison de Oliveira Rodrigues <95246993+Harrison1033@users.noreply.github.com>
2023-08-20 11:32:30 -0300

    Criação de classes

---
+++ codigoJava/CollectionsJava/MapOperacoesBasicas/AgendaContatos/AgendaContatos.java
@@ -0,0 +1,36 @@
+package teste;
+
+import java.util.HashMap;
+import java.util.Map;
+
+//AgendaContatos(Map)
+public class AgendaContatos {
+    //atributo
+    private Map<String, Integer> agendaContatoMap;
+
+    public AgendaContatos() {
+        this.agendaContatoMap = new HashMap<>();
+    }
+
+    public void adicionarContato(String nome, Integer telefone) {
+        agendaContatoMap.put(nome, telefone);
+    }
+
+    public void removerContato(String nome) {
+        if(!agendaContatoMap.isEmpty()) {
+            agendaContatoMap.remove(nome);
+        }
+    }
+
+    public void exibirContatos() {
+        System.out.println(agendaContatoMap);
+    }
+
+    public Integer pesquisarPorNome(String nome) {
+        Integer numeroPorNome = null;
+        if (!agendaContatoMap.isEmpty()) {
+            numeroPorNome = agendaContatoMap.get(nome);
+        }
+        return numeroPorNome;
+    }
+}
---
+++ codigoJava/CollectionsJava/MapOperacoesBasicas/AgendaContatos/Main.java
@@ -0,0 +1,22 @@
+package teste;
+
+//Main (Map)
+public class Main {
+    public static void main(String[] args) {
+        AgendaContatos agendaContatos = new AgendaContatos();
+
+        agendaContatos.adicionarContato("Camila", 123456);
+        agendaContatos.adicionarContato("Camila", 5665);
+        agendaContatos.adicionarContato("Camila Cavalcante", 1111111);
+        agendaContatos.adicionarContato("Camila DIO", 654987);
+        agendaContatos.adicionarContato("Maria Silva", 1111111);
+        agendaContatos.adicionarContato("Camila", 44444);
+
+        agendaContatos.exibirContatos();
+
+        agendaContatos.removerContato("Maria Silva");
+        agendaContatos.exibirContatos();
+
+        System.out.println("O número é: " + agendaContatos.pesquisarPorNome("Camila DIO"));
+    }
+}
