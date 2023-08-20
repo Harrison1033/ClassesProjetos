f8fdb9c91c61ec64ae6665ee8654f05a036aa994
Harrison de Oliveira Rodrigues <95246993+Harrison1033@users.noreply.github.com>
2023-08-20 11:50:06 -0300

    criação da classe AgendaEventosMap

---
+++ codigoJava/CollectionsJava/MapOrdenacao/AgendaEventos/AgendaEventos.java
@@ -0,0 +1,44 @@
+package teste;
+
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+//AgendaEventos(Map)
+public class AgendaEventos {
+    //atributo
+    private Map<LocalDate, Eventos> eventosMap;
+
+    public AgendaEventos() {
+        this.eventosMap = new HashMap<>();
+    }
+
+    public void adicionarEvento(LocalDate data, String nome, String atracao) {
+        //Evento evento = new Evento(nome, atracao);
+        eventosMap.put(data, new Eventos(nome, atracao));
+    }
+
+    public void exibirAgenda() {
+        Map<LocalDate, Eventos> eventosTreeMap = new TreeMap<>(eventosMap);
+        System.out.println(eventosTreeMap);
+    }
+
+    public void obterProximoEvento() {
+    /*Set<LocalDate> dateSet = eventosMap.keySet();
+    Collection<Evento> values = eventosMap.values();*/
+        //eventosMap.get();
+        LocalDate dataAtual = LocalDate.now();
+        LocalDate proximaData = null;
+        Eventos proximoEvento = null;
+        Map<LocalDate, Eventos> eventosTreeMap = new TreeMap<>(eventosMap);
+        for (Map.Entry<LocalDate, Eventos> entry : eventosTreeMap.entrySet()) {
+            if (entry.getKey().isEqual(dataAtual) || entry.getKey().isAfter(dataAtual)) {
+                proximaData = entry.getKey();
+                proximoEvento = entry.getValue();
+                System.out.println("O próximo evento: " + proximoEvento + " acontecerá na data " + proximaData);
+                break;
+            }
+        }
+    }
+}
