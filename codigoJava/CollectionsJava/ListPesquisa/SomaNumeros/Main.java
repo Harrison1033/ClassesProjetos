616c65e7cee10622dae33c2e3d078cd7ecfcc379
Harrison de Oliveira Rodrigues <95246993+Harrison1033@users.noreply.github.com>
2023-08-20 11:16:24 -0300

    Criação da classe Main

---
+++ codigoJava/CollectionsJava/ListPesquisa/SomaNumeros/Main.java
@@ -0,0 +1,23 @@
+package teste;
+
+//Main (List)
+public class Main {
+    public static void main(String[] args) {
+        SomaNumeros somaNumeros = new SomaNumeros();
+
+        somaNumeros.adicionarNumero(5);
+        somaNumeros.adicionarNumero(0);
+        somaNumeros.adicionarNumero(0);
+        somaNumeros.adicionarNumero(-2);
+        somaNumeros.adicionarNumero(10);
+        somaNumeros.exibirNumeros();
+
+        System.out.println("Soma dos números = " + somaNumeros.calcularSoma());
+
+        System.out.println("Maior número = " + somaNumeros.encontrarMaiorNumero());
+
+        System.out.println("Menor número = " + somaNumeros.encontrarMenorNumero());
+
+
+    }
+}
