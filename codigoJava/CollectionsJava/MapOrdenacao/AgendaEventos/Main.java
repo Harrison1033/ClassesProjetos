6f5311863284a98c40fadd020ca03ec97a7831ca
Harrison de Oliveira Rodrigues <95246993+Harrison1033@users.noreply.github.com>
2023-08-21 08:45:02 -0300

    Criação da classe MainMap

---
+++ codigoJava/CollectionsJava/MapOrdenacao/AgendaEventos/Main.java
@@ -0,0 +1,20 @@
+package teste;
+
+import java.time.LocalDate;
+import java.time.Month;
+
+//Main (Map)
+public class Main {
+    public static void main(String[] args) {
+        AgendaEventos agendaEventos = new AgendaEventos();
+        agendaEventos.adicionarEvento(LocalDate.of(2022, Month.JULY, 15), "Evento 1", "Atração 1");
+        agendaEventos.adicionarEvento(LocalDate.of(2022, 7, 9), "Evento 2", "Atração 2");
+        agendaEventos.adicionarEvento(LocalDate.of(2000, Month.JANUARY, 10), "Evento 3", "Atração 3");
+        agendaEventos.adicionarEvento(LocalDate.of(2023, Month.JULY, 14), "Evento 5", "Atração 5");
+        agendaEventos.adicionarEvento(LocalDate.of(2024, Month.SEPTEMBER, 20), "Evento 6", "Atração 6");
+
+        agendaEventos.exibirAgenda();
+
+        agendaEventos.obterProximoEvento();
+    }
+}
