47de0a6f8737828fee8f5efe98e07eeea4fccbcf
Harrison de Oliveira Rodrigues <95246993+Harrison1033@users.noreply.github.com>
2023-08-28 08:32:18 -0300

    Criação da classe MainMap

---
+++ codigoJava/CollectionsJava/MapPesquisa/EstoqueDeProdutos/Main.java
@@ -0,0 +1,20 @@
+package teste;
+//Main (Map)
+public class Main {
+    public static void main(String[] args) {
+        EstoqueProdutos estoque = new EstoqueProdutos();
+        estoque.exibirProdutos();
+
+        estoque.adicionarProduto(1L, "Produto A", 1, 50.0);
+        estoque.adicionarProduto(2L, "Produto B", 5, 11.0);
+        estoque.adicionarProduto(3L, "Produto C", 3, 10.0);
+        estoque.adicionarProduto(3L, "Produto D", 2, 4.0);
+
+        estoque.exibirProdutos();
+
+        System.out.println("Valor total do estoque: R$" + estoque.calcularValorTotalEstoque());
+        System.out.println("Produto mais caro: " + estoque.obterProdutoMaisCaro());
+        System.out.println("Produto com maior quantidade em valor no estoque: " + estoque.obterProdutoMaiorQuantidadeValorTotalNoEstoque());
+
+    }
+}
