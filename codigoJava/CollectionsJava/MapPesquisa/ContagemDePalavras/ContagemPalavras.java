98ca8d76f5129fcd5d8b3686cb30cd831f80511e
Harrison de Oliveira Rodrigues <95246993+Harrison1033@users.noreply.github.com>
2023-08-29 10:42:54 -0300

    Criação da classe ContagemPalavrasMap

---
+++ codigoJava/CollectionsJava/MapPesquisa/ContagemDePalavras/ContagemPalavras.java
@@ -0,0 +1,45 @@
+package teste;
+
+import java.util.*;
+
+//ContagemPalavras(Map)
+public class ContagemPalavras {
+    //atributo
+    private Map<String, Integer> palavras;
+
+    public ContagemPalavras() {
+        this.palavras = new HashMap<>();
+    }
+
+    public void adicionarPalavras(String linguagem, Integer contagem) {
+        palavras.put(linguagem, contagem);
+    }
+
+    public void removerPalavra(String palavra) {
+        if (!palavras.isEmpty()) {
+            palavras.remove(palavra);
+        } else {
+            System.out.println("O Map está vazio.");
+        }
+    }
+
+    public int exibirContagemPalavras() {
+        int contagemTotal = 0;
+        for (int contagem : palavras.values()) {
+            contagemTotal += contagem;
+        }
+        return contagemTotal;
+    }
+
+    public String encontrarPalavrasMaisFrequente() {
+        String linguagemMaisFrequente = null;
+        int maiorContagem = 0;
+        for (Map.Entry<String, Integer> entry : palavras.entrySet()) {
+            if (entry.getValue() > maiorContagem) {
+                maiorContagem = entry.getValue();
+                linguagemMaisFrequente = entry.getKey();
+            }
+        }
+        return linguagemMaisFrequente;
+    }
+}
