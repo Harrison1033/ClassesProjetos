f664d8203402472802662bcf1569141995f06519
Harrison de Oliveira Rodrigues <95246993+Harrison1033@users.noreply.github.com>
2023-09-15 18:11:58 -0300

    Criação da classe test ContaTest

---
+++ codigoJava/TesteUnitario/Test/ContaTest.java
@@ -0,0 +1,22 @@
+public class ContaTest {
+
+    @Test
+    void validaSaldo() {
+        Conta conta = new Conta("123456", 100);
+        Assertions.assertNotNull(conta);
+
+        conta.lancaCredito(50);
+
+        Assertions.assertEquals(150, conta.getSaldo());
+
+        conta.lancaDebito(50);
+
+        Assertions.assertEquals(100, conta.getSaldo());
+
+        Assertions.assertNotEquals(101, conta.getSaldo());
+
+        conta = null;
+        Assertions.assertNull(conta);
+    }
+
+}
