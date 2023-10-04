6c94f01b1d9732542929d6b5d67f74bec4b8deef
Harrison de Oliveira Rodrigues <95246993+Harrison1033@users.noreply.github.com>
2023-10-04 14:15:13 -0300

    Criação da Classe Main.java

---
+++ Projetos/ProjetoAgendaTxt/Main.java
@@ -0,0 +1,17 @@
+package agenda;
+
+import java.io.IOException;
+
+//Classe que lança a aplicação
+public class Main {
+	/**
+	 * Método main
+	 * @param args
+	 * @throws IOException
+	 */
+	public static void main(String[] args) throws IOException {
+		// Instancia a aplicação e a inicia
+		Aplicacao aplicacao = new Aplicacao();
+		aplicacao.iniciar();
+	}
+}
