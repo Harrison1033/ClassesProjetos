f18285e32919c44d098fa4ec972e6057d47b4f89
Harrison de Oliveira Rodrigues <95246993+Harrison1033@users.noreply.github.com>
2023-11-06 03:16:41 -0300

    Modify2

--- codigoJava/CollectionsJava/MapOrdenacao/AgendaEventos/Main.java
+++ codigoJava/CollectionsJava/MapOrdenacao/AgendaEventos/Main.java
@@ -7,7 +7,7 @@ import java.time.Month;
 public class Main {
     public static void main(String[] args) {
         AgendaEventos agendaEventos = new AgendaEventos();
-        agendaEventos.adicionarEvento(LocalDate.of(2022, Month.JULY, 15), "Evento 1", "Atração 1");
+        agendaEventos.adicionarEvento(LocalDate.of(2022, Month.JULY, 14), "Evento 1", "Atração 1");
         agendaEventos.adicionarEvento(LocalDate.of(2022, 7, 9), "Evento 2", "Atração 2");
         agendaEventos.adicionarEvento(LocalDate.of(2000, Month.JANUARY, 10), "Evento 3", "Atração 3");
         agendaEventos.adicionarEvento(LocalDate.of(2023, Month.JULY, 14), "Evento 5", "Atração 5");
