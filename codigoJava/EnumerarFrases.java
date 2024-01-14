5e5be15ea78a510fa36fae7d644e487102f43580
Harrison de Oliveira Rodrigues <95246993+Harrison1033@users.noreply.github.com>
2024-01-14 10:49:05 -0300

    Modify 05

--- codigoJava/EnumerarFrases.java
+++ codigoJava/EnumerarFrases.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class ClasseCinco {
+public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int lineNumber = 0;
