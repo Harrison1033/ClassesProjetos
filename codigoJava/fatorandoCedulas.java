efac7f49df397d6d37aa774c399cdeb77293640e
Harrison de Oliveira Rodrigues <95246993+Harrison1033@users.noreply.github.com>
2024-01-20 21:57:25 -0300

    Refatoração

--- codigoJava/fatorandoCedulas.java
+++ codigoJava/fatorandoCedulas.java
@@ -1,7 +1,7 @@
 package br.com.harrison.principal;
 import java.util.Scanner;
 //Fatorando cédula
-public class TesteMain01 {
+public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Digite o valor a ser fatorado");
