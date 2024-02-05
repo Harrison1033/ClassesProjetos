6e4f34844258c8368834e160682513687f72c23a
Harrison de Oliveira Rodrigues <95246993+Harrison1033@users.noreply.github.com>
2024-02-05 17:33:09 -0300

    Main modify

--- codigoJava/maiorDeTres.java
+++ codigoJava/maiorDeTres.java
@@ -2,7 +2,7 @@ package br.com.harrison.principal;
 
 import java.util.Scanner;
 
-public class TesteMain01 {
+public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter the first number: ");
