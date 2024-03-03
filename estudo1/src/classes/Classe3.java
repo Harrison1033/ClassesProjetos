7f6b01b0b44e9251158dba151764588cbf95af59
Harrison de Oliveira Rodrigues <95246993+Harrison1033@users.noreply.github.com>
2024-03-03 12:38:55 -0300

    Modificação de código

--- estudo1/src/classes/Classe3.java
+++ estudo1/src/classes/Classe3.java
@@ -1,14 +1,25 @@
 package classes;
 
-import java.util.stream.IntStream;
-
 public class Classe3 {
-    //protected int num1=0;
-    protected int num2=3;
-
+    public int soma(int a, int b){
+        return a + b;
+    }
+    protected int multiplica(int a, int b){
+        return a * b;
+    }
+    protected int subtrai (int a, int b){
+        return a - b;
+    }
+    private double divide (int x, int y){
+        if (y == 0){
+           return 0;
+        }
+        return x / y;
+    }
     public void imprimir(){
-        IntStream.rangeClosed(0, 10).
-                mapToObj(i -> num2 + " X " + i + " = " + num2 * i).
-                forEach(System.out::println);
+        System.out.println("Soma: "+soma(3,9));
+        System.out.println("Subtrai: " + subtrai(14,9));
+        System.out.println("Multiplica: " + multiplica(7,7));
+        System.out.println("Divide: " + divide(27,3));
     }
 }
