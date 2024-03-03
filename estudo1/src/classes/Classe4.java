e8c81fdccf2456f5eb25a41564c669b39b6c6c67
Harrison de Oliveira Rodrigues <95246993+Harrison1033@users.noreply.github.com>
2024-03-03 18:23:45 -0300

    Modify03

--- estudo1/src/classes/Classe4.java
+++ estudo1/src/classes/Classe4.java
@@ -1,4 +1,22 @@
 package classes;
 
 public class Classe4 {
+    private String nome;
+    static {
+        System.out.println("Eis abaixo o nome: ");
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Classe4: " +
+                "Nome = " + nome  ;
+    }
 }
--- estudo1/src/teste/Main.java
+++ estudo1/src/teste/Main.java
@@ -2,16 +2,20 @@ package teste;
 
 import classes.Classe2;
 import classes.Classe3;
+import classes.Classe4;
 
 public class Main {
     public static void main(String[] args) {
         Classe2 classe1 = new Classe2(23,"Alita Meyer",1.69,2339);
         Classe2 classe2 = new Classe2(19,"Alberto Roberto",1.78,2300);
         Classe3 classe3 = new Classe3();
+        Classe4 classe4 = new Classe4();
         classe2.imprimir();
         System.out.println("---------------------------------------");
         classe1.imprimir();
         System.out.println("---------------------------------------");
         classe3.imprimir();
+        classe4.setNome("Fredie Nendile");
+        System.out.println(classe4.toString());
     }
 }
