5a56bbcb3178bf9bd58386260425b778dfcce55e
Harrison de Oliveira Rodrigues <95246993+Harrison1033@users.noreply.github.com>
2024-03-18 14:53:15 -0300

    Modify08

--- estudo1/src/classes/Classe1.java
+++ estudo1/src/classes/Classe1.java
@@ -1,5 +1,6 @@
 package classes;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 //Com Scanner
@@ -7,13 +8,18 @@ public class Classe1 {
     Scanner scan = new Scanner(System.in);
 
     public void imprimir(){
-        System.out.println("\nDigite um número inteiro");
-        int num = scan.nextInt();
-       // System.out.println(" ");
-        for ( int i = 0;i<=num; i++) {
-            if (i%2 !=0) {
-                System.out.print(i + " ");
-            }
+        try {
+            System.out.println("\nDigite um número inteiro");
+            int num = scan.nextInt();
+            // System.out.println(" ");
+            for ( int i = 0;i<=num; i++) {
+                if (i%2 !=0) {
+                    System.out.print(i + " ");
+                }
+        }
+
+        }catch (IllegalStateException | InputMismatchException e){
+            System.out.println("Erro ao digitar o número");
         }
     }
 }
