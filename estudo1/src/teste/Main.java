a04aee971e08bd1c0d6cd8b17c17050e9ac9ccd2
Harrison de Oliveira Rodrigues <95246993+Harrison1033@users.noreply.github.com>
2024-03-23 20:59:53 -0300

    Modify09

--- estudo1/src/teste/Main.java
+++ estudo1/src/teste/Main.java
@@ -1,5 +1,5 @@
 package teste;
-//momodify09
+//momodify10
 /**
  * Programa Destinado para rever temas já aprendidos
  * em cada classe terá uma descrição do que ela represnta
@@ -11,12 +11,7 @@ package teste;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-//import classes.Classe1;
-//import classes.Classe2;
-//import classes.Classe3;
-//import classes.Classe4;
-
-
+//Refazer essa classe depois de entender ela bem
 class User {
     private final int id;
     private final String name;
@@ -38,17 +33,17 @@ class User {
 public class Main {
     private static final List<User> users = new ArrayList<>();
 
-    // TODO: Implementar o padrão Singleton para garantir que haja apenas uma instância do gerenciador de usuários
+    // FIXME: Implementar o padrão Singleton para garantir que haja apenas uma instância do gerenciador de usuários
 
     public static void addUser(String name) {
 
-        // TODO("Implementar a lógica de adicionar um novo usuário na lista mutável $users.")
+    // TODO("Implementar a lógica de adicionar um novo usuário na lista mutável $users.")
         users.add(new User(users.size() + 1, name));
     }
 
     public static void listUsers() {
 
-        // TODO("Implementar a impressão dos $users, seguindo o padrão definido no enunciado.")
+     // TODO("Implementar a impressão dos $users, seguindo o padrão definido no enunciado.")
         for (User user : users) {
             System.out.println(user.getName());
         }
@@ -56,13 +51,16 @@ public class Main {
 
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
+        int quantity;
+        System.out.println("Digite um número:");
+        quantity = scan.nextInt();
+        scan.nextLine();  // Consumir a quebra de linha após a leitura do número
 
-        int quantity = scanner.nextInt();
-        scanner.nextLine();  // Consumir a quebra de linha após a leitura do número
-       
         for (int i = 1; i <= quantity; i++) {
-            String name = scanner.nextLine();
+            String name;
+            System.out.println("Digite o nome:");
+            name = scan.nextLine();
             addUser(i + " - " + name);
         }
 
