44cad57ff5c05c00f98714f6c0937d43e310783b
Harrison de Oliveira Rodrigues <95246993+Harrison1033@users.noreply.github.com>
2024-03-31 17:28:27 -0300

    Modificando

--- codigoJava/ConversaoMoedaStrategy/Main.java
+++ codigoJava/ConversaoMoedaStrategy/Main.java
@@ -9,12 +9,16 @@ public class Main {
         System.out.println("Digite um valor a ser convertido: ");
         double input = Double.parseDouble(scan.nextLine());
         scan.close();
+		
         OldCurrencyConverter oldConverter = new OldCurrencyConverter();
         CurrencyAdapter adapter = new CurrencyAdapter(oldConverter);
+		
         // TODO: Chamar o método para converter USD para a nova moeda desejada (por exemplo, JPY)
         double inputEUR = adapter.convertUSDtoEUR(input);
+		
         // TODO: Imprimir o resultado da conversão
         System.out.println("USD: " + input);
+		
         // TODO: Imprimir o resultado da conversão com a nova moeda (por exemplo, JPY)
         System.out.println("EUR: " + inputEUR);
     }
