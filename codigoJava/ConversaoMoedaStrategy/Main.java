fedc22a0148fd51995dc44df9d197bcb29805127
Harrison de Oliveira Rodrigues <95246993+Harrison1033@users.noreply.github.com>
2024-04-04 17:39:39 -0300

    Modificação 3

--- codigoJava/ConversaoMoedaStrategy/Main.java
+++ codigoJava/ConversaoMoedaStrategy/Main.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+		
         Scanner scan = new Scanner(System.in);
 
         System.out.println("Digite um valor a ser convertido: ");
