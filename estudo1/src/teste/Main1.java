2a3f275ac1d5b4da6b49efeb4402514388f1742c
Harrison de Oliveira Rodrigues <95246993+Harrison1033@users.noreply.github.com>
2024-04-30 18:31:56 -0300

    Testando método

--- estudo1/src/teste/Main1.java
+++ estudo1/src/teste/Main1.java
@@ -6,6 +6,19 @@ import java.util.Scanner;
 public class Main1 {
     public static void main(String[] aargs) {
         Scanner scan = new Scanner(System.in);
-
+        int n1;
+        int n2;
+        System.out.println("Digite o primeiro númro: ");
+        n1 = scan.nextInt();
+        System.out.println("Digite o segundo número: ");
+        n2 = scan.nextInt();
+        Teste teste = new Teste();
+        System.out.println(" = "  + teste.multiplicaDoisNumeros(n1,n2));
+    }
+    public static class Teste{
+        public int multiplicaDoisNumeros(int numero1, int numero2){
+            System.out.print(numero1 + " X " + numero2);
+            return numero1 * numero2;
+        }
     }
 }
