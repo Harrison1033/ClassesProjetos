51a12a8cf9c31a725cc1a3d58a864a55e2d5f10d
Harrison de Oliveira Rodrigues <95246993+Harrison1033@users.noreply.github.com>
2024-05-01 20:02:47 -0300

    Refatoração de um projeto

--- estudo1/src/classes/Classe2.java
+++ estudo1/src/classes/Classe2.java
@@ -1,5 +1,19 @@
 package classes;
-//
+
+import java.util.Scanner;
+
+//Main1 Cliente(nome)
 public class Classe2  {
+    Scanner scan = new Scanner(System.in);
+    Classe1 classe1 = new Classe1();
+    public void imprimir(){
+        System.out.println("Digite o nome");
+        String nomeCliente = scan.next();
+        System.out.println("Digite o sobre nome");
+        String sobrenomeCliente = scan.next();
+        classe1.imprimir();
+        System.out.print("Olá " + nomeCliente + " " + sobrenomeCliente+" obrigado por criar uma conta em nosso banco, ");
+        System.out.println("sua agência é " + classe1.agencia + ",conta "+ classe1.numero + " e saldo " + classe1.getSaldo() + " á está disponível para saque");
+    }
 
 }
--- estudo1/src/teste/Main1.java
+++ estudo1/src/teste/Main1.java
@@ -1,23 +1,13 @@
 package teste;
 
-import java.util.Scanner;
+import classes.Classe1;
+import classes.Classe2;
 
+import java.util.Scanner;
+//ContaTerminal
 public class Main1 {
     public static void main(String[] aargs) {
-
-        Teste teste = new Teste();
-        System.out.println(" = "  + teste.multiplicaDoisNumeros());
-    }
-    public static class Teste{
-        public int multiplicaDoisNumeros(){
-            Scanner scan = new Scanner(System.in);
-            System.out.println("Digite o primeiro númro: ");
-            Integer numero1 = scan.nextInt();
-            System.out.println("Digite o segundo número: ");
-            Integer numero2 = scan.nextInt();
-            System.out.print(numero1 + " X " + numero2);
-            return numero1 * numero2;
-
-        }
+        Classe2 classe2 = new Classe2();
+        classe2.imprimir();
     }
 }
