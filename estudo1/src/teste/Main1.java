05cfb987ac9a43e64fa9c5ed7b2033bd00417ff1
Harrison de Oliveira Rodrigues <95246993+Harrison1033@users.noreply.github.com>
2024-05-06 14:10:01 -0300

    Revisando exceptions

--- estudo1/src/teste/Main1.java
+++ estudo1/src/teste/Main1.java
@@ -1,18 +1,20 @@
 package teste;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 //
 public class Main1 {
     public static void main(String[] aargs) {
         Scanner scan = new Scanner(System.in);
+        try {
         System.out.println("Digite um número de 1 a 7");
         int numero = scan.nextInt();
       String[] nomes = new String[]{"João ", "Maria ", "Nícolas ", "Betty", "Walter ", "Joana "};
       for (int i = 0; i<nomes.length;i++){
           System.out.println("Matrícula: " + (i+1) + " Nome: " + nomes[i]);
       }
-      switch (numero){
+      switch (numero) {
           case 1 -> System.out.println("Domingo");
           case 2 -> System.out.println("Segunda");
           case 3 -> System.out.println("Terça");
@@ -21,7 +23,10 @@ public class Main1 {
           case 6 -> System.out.println("Sexta");
           case 7 -> System.out.println("Sábado");
           default -> System.out.println("Valor Inválido: ");
-
       }
+
+      }catch (InputMismatchException e){
+            System.out.println("Erro. Digite um número inteiro... ");
+        }
     }
 }
