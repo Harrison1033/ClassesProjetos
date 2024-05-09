45d3e821ee18a95d689ff0f0c6de22f07a29c0a4
Harrison de Oliveira Rodrigues <95246993+Harrison1033@users.noreply.github.com>
2024-05-09 14:19:24 -0300

    Dependencias de métodos

--- estudo1/src/teste/Main1.java
+++ estudo1/src/teste/Main1.java
@@ -6,27 +6,23 @@ import java.util.Scanner;
 //
 public class Main1 {
     public static void main(String[] aargs) {
-        Scanner scan = new Scanner(System.in);
-        try {
-        System.out.println("Digite um número de 1 a 7");
-        int numero = scan.nextInt();
-      String[] nomes = new String[]{"João ", "Maria ", "Nícolas ", "Betty", "Walter ", "Joana "};
-      for (int i = 0; i<nomes.length;i++){
-          System.out.println("Matrícula: " + (i+1) + " Nome: " + nomes[i]);
-      }
-      switch (numero) {
-          case 1 -> System.out.println("Domingo");
-          case 2 -> System.out.println("Segunda");
-          case 3 -> System.out.println("Terça");
-          case 4 -> System.out.println("Quarta");
-          case 5 -> System.out.println("Quinta");
-          case 6 -> System.out.println("Sexta");
-          case 7 -> System.out.println("Sábado");
-          default -> System.out.println("Valor Inválido: ");
-      }
-
-      }catch (InputMismatchException e){
-            System.out.println("Erro. Digite um número inteiro... ");
-        }
+        System.out.println("Entrando no método Main");
+        a();
+        System.out.println("Finalizando o método main");
+    }
+    static void a(){
+        System.out.println("Entrando no método A");
+        for (int i = 0; i < 5; i++) System.out.print(i + ", ");
+        b();
+        System.out.println("Finalizando o método A");
+    }
+    static void b(){
+        System.out.println("Entrando no método B");
+        c();
+        System.out.println("Finalizando o método B");
+    }
+    static void c(){
+        System.out.println("Entrando no método C");
+        System.out.println("Finalizando o métoso C");
     }
 }
