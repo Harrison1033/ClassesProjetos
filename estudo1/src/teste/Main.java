077e3c55c23ffd9875f68da392ca8faae265abe2
Harrison de Oliveira Rodrigues <95246993+Harrison1033@users.noreply.github.com>
2024-05-10 16:48:16 -0300

    Modificação05

--- estudo1/src/teste/Main.java
+++ estudo1/src/teste/Main.java
@@ -1,5 +1,8 @@
 package teste;
-//momodify10
+//
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /**
  * Programa Destinado para rever temas já aprendidos
  * em cada classe terá uma descrição do que ela represnta
@@ -9,6 +12,32 @@ package teste;
  */
 public class Main {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String nome;
+        double num1;
+        double num2;
+        try {
+        System.out.println("Digite a operação");
+        nome = scan.nextLine();
+        System.out.println("Digite um número: ");
+        num1 = scan.nextDouble();
+        System.out.println("Digite outro número: ");
+        num2 = scan.nextDouble();
+
+            if (nome.equalsIgnoreCase("Soma") || nome.equalsIgnoreCase("+")) {
+                System.out.printf("%.2f",num1 + num2);
+            } else if (nome.equalsIgnoreCase("subtração") || nome.equalsIgnoreCase("-")) {
+                System.out.printf("%.2f",num1 - num2);
+            } else if (nome.equalsIgnoreCase("multiplicação") || nome.equalsIgnoreCase("*")) {
+                System.out.printf("%.2f", num1 * num2);
+            } else if (nome.equalsIgnoreCase("Divisão") || nome.equalsIgnoreCase("/")) {
+                System.out.printf("%.2f", num1 / num2);
+            } else {
+                System.out.println("Opção inválida:");
+            }
+        }catch (InputMismatchException e){
+            System.out.println("Digite somente números: -> " + e);
+        }
 
     }
 }
