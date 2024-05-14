f09b2423bf58212f4b454260dae31f4f0b6ce271
Harrison de Oliveira Rodrigues <95246993+Harrison1033@users.noreply.github.com>
2024-05-14 12:09:59 -0300

    Modificação de desafio de código

--- estudo1/src/teste/Main1.java
+++ estudo1/src/teste/Main1.java
@@ -1,10 +1,31 @@
 package teste;
 
-import java.util.InputMismatchException;
 import java.util.Scanner;
-//Métodos intrelassados
+
+//Desafio de código(Contador)
 public class Main1 {
     public static void main(String[] aargs) {
-       
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Digite o primeiro número");
+        int numero1 = scan.nextInt();
+        System.out.println("Digite o segundo número");
+        int numero2 = scan.nextInt();
+        try {
+            if (numero1 > numero2) {
+                //A exceção IllegalArgumentException é usada para indicar que o segundo número deve ser maior que o primeiro.
+                throw new IllegalArgumentException("O segundo número deve ser maior que o primeiro.");
+            }
+
+            contar(numero1, numero2);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Erro: " + e.getMessage());
+        }
+    }
+
+    static void contar(int numero1, int numero2) {
+        //O loop for é usado para imprimir os números entre numero1 e numero2.
+        for (int i = numero1; i < numero2; i++) {
+            System.out.println(i);
+        }
     }
 }
