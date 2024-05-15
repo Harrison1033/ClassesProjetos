b5d32161f98ec23871ebe8157378ab47b770ea68
Harrison de Oliveira Rodrigues <95246993+Harrison1033@users.noreply.github.com>
2024-05-15 12:05:16 -0300

    Modificação de código01

--- estudo1/src/teste/Main.java
+++ estudo1/src/teste/Main.java
@@ -12,9 +12,19 @@ import java.util.Scanner;
  * ao final, espero está preparado para um projeto bem complexo
  */
 public class Main {
-
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        int idade = scanner.nextInt();
+
+//  Verifique se a idade é maior ou igual a 18 e imprima uma mensagem informando que o usuário é elegível para criar uma conta bancária:
+        if (idade >= 18){
+            System.out.println("Voce esta elegivel para criar uma conta bancaria.");
+        }else {
+            System.out.println("Voce nao esta elegivel para criar uma conta bancaria.");
+        }
+
+// Fechamos o objeto Scanner para liberar os recursos:
+        scanner.close();
     }
 }
-
