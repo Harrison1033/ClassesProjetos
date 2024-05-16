259db2a3f6ab16b0680864ec4f12af66c488152e
Harrison de Oliveira Rodrigues <95246993+Harrison1033@users.noreply.github.com>
2024-05-16 09:12:20 -0300

    Refatoração de código

--- estudo1/src/teste/Main.java
+++ estudo1/src/teste/Main.java
@@ -1,9 +1,6 @@
 package teste;
 //Condições, Loops e Exceções na Prática com Java
-
-import java.util.InputMismatchException;
 import java.util.Scanner;
-
 /**
  * Programa Destinado para rever temas já aprendidos
  * em cada classe terá uma descrição do que ela represnta
@@ -14,26 +11,20 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        System.out.println("Informe o valor do limite diário:");
         double limiteDiario = scanner.nextDouble();
-
 // Crie um loop 'for' para iterar sobre os saques:
         for (int i = 1; i <= 3; i++) {
-
             // Solicite ao usuário o valor do saque:
              System.out.println("Informe o valor do saque:");
             double valorSaque = scanner.nextDouble();
-
             // Verifique se o valor do saque é zero, encerrando as transações:
             if (valorSaque == 0) {
                 System.out.println("Transacoes encerradas.");
                 break;
-            }
-
-            // Se o valor do saque não ultrapassar o limite diário, subtraia o valor do saque do limite diário:
-            if (valorSaque <= limiteDiario) {
+                // Se o valor do saque não ultrapassar o limite diário, subtraia o valor do saque do limite diário:
+                } else if (valorSaque <= limiteDiario) {
                 limiteDiario -= valorSaque;
-
                 // Informe que o saque foi realizado e mostre o limite restante:
                 System.out.print("Saque realizado. ");
                 System.out.printf("Limite restante: %.1f\n", limiteDiario);
@@ -43,7 +34,6 @@ public class Main {
                 break;
             }
         }
-
         // Fechamos o Scanner para evitar vazamento de recursos:
         scanner.close();
     }
