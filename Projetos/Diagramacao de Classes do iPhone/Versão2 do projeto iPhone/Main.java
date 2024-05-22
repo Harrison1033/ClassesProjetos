b54cc191cc1e3d8f25858645b67d7b8736c15cb3
Harrison de Oliveira Rodrigues <95246993+Harrison1033@users.noreply.github.com>
2024-05-22 15:55:03 -0300

    criação da classe Main.java

--- Projetos/Diagramacao de Classes do iPhone/Versão2 do projeto iPhone/Main.java
+++ Projetos/Diagramacao de Classes do iPhone/Versão2 do projeto iPhone/Main.java
@@ -3,27 +3,27 @@ package classes;
 import java.util.Scanner;
 
 //Projeto iPhone utilizando das classes de 2 a 5
-public class Classe1 {
+public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        Classe2 classe2 = new Classe2();
+        IPhone iPhone = new IPhone();
         int opcao = 0;
         while (opcao == 0) {
             System.out.println("Qual opção Gostaria de Utilizar?");
             System.out.println("(a) Reprodutor musical\n(b) Telefone\n(c) Internet");
             String escolha = scan.next();
             if (escolha.equalsIgnoreCase("a")) {
-                classe2.selecionarMusica();
-                classe2.tocar();
-                classe2.pausar();
+                iPhone.selecionarMusica();
+                iPhone.tocar();
+                iPhone.pausar();
             } else if (escolha.equalsIgnoreCase("b")) {
-                classe2.atender();
-                classe2.ligar();
-                classe2.iniciarCorreioDeVoz();
+                iPhone.atender();
+                iPhone.ligar();
+                iPhone.iniciarCorreioDeVoz();
             } else if (escolha.equalsIgnoreCase("c")) {
-                classe2.adicionarAba();
-                classe2.atualizarPagina();
-                classe2.exibir();
+                iPhone.adicionarAba();
+                iPhone.atualizarPagina();
+                iPhone.exibir();
             } else {
                 System.out.println("Opção inválida: ");
             }
@@ -32,4 +32,3 @@ public class Classe1 {
         }
     }
 }
-
