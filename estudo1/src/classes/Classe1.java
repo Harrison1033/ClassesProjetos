563ad569480ed5435166e18aab945d21c78799d4
Harrison de Oliveira Rodrigues <95246993+Harrison1033@users.noreply.github.com>
2024-05-24 14:18:15 -0300

    Modificação e exemplo de classe com list e lambda

--- estudo1/src/classes/Classe1.java
+++ estudo1/src/classes/Classe1.java
@@ -1,9 +1,20 @@
 package classes;
 
-//
+import java.util.ArrayList;
+import java.util.List;
+
+//Exemplo de list que imprime o primeiro resultado utilizando o lambda
+
 public class Classe1 {
     public static void main(String[] args) {
+        List<String> lista = new ArrayList<>();
+        lista.add("Laranja");
+        lista.add("Limão");
+        lista.add("Banana");
+        lista.add("Acerola");
 
+        lista.stream().findFirst().ifPresent(System.out::println);
+        System.out.println(lista);
     }
 }
 
