0389f7f5c6c2d37e18174f73b406bcbeae37d8bb
Harrison de Oliveira Rodrigues <95246993+Harrison1033@users.noreply.github.com>
2024-06-08 20:37:29 -0300

    Modificação02.3

--- Projetos/Diagramacao de Classes do iPhone/Versão2 do projeto iPhone/Main.java
+++ Projetos/Diagramacao de Classes do iPhone/Versão2 do projeto iPhone/Main.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+		
         Scanner scan = new Scanner(System.in);
         IPhone iPhone = new IPhone();
 		
