787396bba3fefeb0154698c9c6e8ed30ac448eee
Harrison de Oliveira Rodrigues <95246993+Harrison1033@users.noreply.github.com>
2024-07-12 16:18:54 -0300

    Modificação04.3

--- codigoJava/verificacaoDeOitoDigitos.java
+++ codigoJava/verificacaoDeOitoDigitos.java
@@ -9,6 +9,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+		
         Scanner scanner = new Scanner(System.in);
 
 // TODO: Inicialize um bloco try-catch para capturar exceções:
