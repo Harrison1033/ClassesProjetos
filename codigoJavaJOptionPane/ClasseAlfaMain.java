2f61660536fcd3d510f0973a02f33819c4b6b07e
Harrison de Oliveira Rodrigues <95246993+Harrison1033@users.noreply.github.com>
2025-01-23 14:46:29 -0300

    Criacao da classe Main de novo estudo

---
+++ codigoJavaJOptionPane/ClasseAlfaMain.java
@@ -0,0 +1,32 @@
+import javax.swing.*;
+//import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+//        Scanner scan = new Scanner(System.in);
+        float num;
+        String aux;
+        ClassA classA = new ClassA();//Comprimento e Área
+        ClassB classB = new ClassB();//Inclusão de frase no arquivo.txt
+        ClassC classC = new ClassC();//Maior de idade ou não(Exception personalizado)
+        ClassD classD = new ClassD();//Temporizador H m s
+        try {
+            aux = JOptionPane.showInputDialog("Digite 1 p/ ClassA, 2 p/ ClassB, 3 p/ ClassC e 4 p/ ClassD: ");
+            num = Float.parseFloat(aux);
+            if (num == 1) {
+                classA.imprimir();
+            } else if (num == 2) {
+                classB.imprimir();
+            } else if (num == 3) {
+                classC.imprimir();
+            } else if (num == 4) {
+                classD.imprimir();
+            } else {
+                JOptionPane.showMessageDialog(null, "Opção inválida: ");
+            }
+        } catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(null, "Erro, Digite somente números: " + e.toString());
+        }
+        System.exit(0);
+    }
+}
