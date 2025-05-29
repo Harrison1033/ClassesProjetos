4d33b7867e5d1d3f6b2c00d1c6bf12a070c70d5b
Harrison1033 <harrisonoliveira77@gmail.com>
2025-05-29 09:43:36 -0300

    Criação da classe NormalZombie.java

---
+++ Projetos/JogoTeste/NormalZombie.java
@@ -0,0 +1,7 @@
+import java.awt.Color;
+
+public class NormalZombie extends Zombie {
+    public NormalZombie(int x, int y) {
+        super(x, y, 1, 2, Color.GREEN, "Zumbi 00.png");  // ajuste o nome do arquivo conforme necessário
+    }
+}
