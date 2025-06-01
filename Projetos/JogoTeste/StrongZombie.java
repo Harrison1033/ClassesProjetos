a0f5c1ce346b47215d634917a6a659686810971a
Harrison1033 <harrisonoliveira77@gmail.com>
2025-06-01 16:03:53 -0300

    Criação da classe StrongZombie.java

---
+++ Projetos/JogoTeste/StrongZombie.java
@@ -0,0 +1,7 @@
+import java.awt.Color;
+
+public class StrongZombie extends Zombie {
+    public StrongZombie(int x, int y) {
+        super(x, y, 3, 1, Color.RED, "Zumbi 01.png");// lento e vermelho
+    }
+}
