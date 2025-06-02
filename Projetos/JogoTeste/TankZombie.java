21fa44e5a3f782e21e219ccf642cc6f799cafd37
Harrison1033 <harrisonoliveira77@gmail.com>
2025-06-02 12:03:08 -0300

    Criação da classe TankZombie.java

---
+++ Projetos/JogoTeste/TankZombie.java
@@ -0,0 +1,7 @@
+import java.awt.Color;
+
+public class TankZombie extends Zombie {
+    public TankZombie(int x, int y) {
+        super(x, y, 5, 1, Color.BLUE, "Zumbi 00.png");//is lento (velocidade 1) e azul
+    }
+}
