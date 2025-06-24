8c96c95a8d49fd02d722208d06448d93e72bda24
Harrison1033 <harrisonoliveira77@gmail.com>
2025-06-24 13:22:07 -0300

    Troca de zumbi de 00 para o 03

--- Projetos/JogoTeste/TankZombie.java
+++ Projetos/JogoTeste/TankZombie.java
@@ -2,6 +2,6 @@ import java.awt.Color;
 
 public class TankZombie extends Zombie {
     public TankZombie(int x, int y) {
-        super(x, y, 5, 1, Color.BLUE, "Zumbi 00.png");//is lento (velocidade 1) e azul
+        super(x, y, 5, 1, Color.BLUE, "Zumbi 03.png");//is lento (velocidade 1) e azul
     }
 }
